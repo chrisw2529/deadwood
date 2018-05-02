6d5bcede69aee91bcaa553a0af29327f2615f367
tonecodez <tonydinhh@gmail.com>
2018-05-02 16:03:04 -0700

    scene class

---
+++ scene.java
@@ -0,0 +1,23 @@
+public class scene
+{
+    String name;
+    int shotMarker;
+    boolean isWrapped = false;
+
+    public static void act(player player, scene name)
+    {
+        
+    }
+
+    public static void rehearse(player player, scene name)
+    {
+
+    }
+
+    public static wrapScene(boolean isWrapped)
+    {
+
+    }
+
+
+}
