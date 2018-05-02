05c416debac3cddd33a83acd131134343a9e119f
tonecodez <tonydinhh@gmail.com>
2018-05-02 15:17:20 -0700

    skeleton

--- player.java
+++ player.java
@@ -1,3 +1,29 @@
 public class player{
-  
+
+    int fame = 0;
+    int cash = 0;
+    int rank = 0;
+    int rehearsalTok = 0;
+    String name;
+    boolean turn = false;
+
+    public player(String name)
+    {
+        this.name = name;
+
+
+    }
+
+    public static void move(player player)
+    {
+
+
+    }
+
+    public static void lvlUp(player player)
+    {
+
+
+    }
+
 }
