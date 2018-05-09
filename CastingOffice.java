7f6d64a22975ec07102433782c85d3d904ccd934
dinht2 <dinht2@wwu.edu>
2018-05-09 16:53:10 -0700

    player.java

--- CastingOffice.java
+++ CastingOffice.java
@@ -5,8 +5,39 @@ public class CastingOffice extends Space{
       super(spaceID, name);
   }
 
-  public static void levelUp(Player player){
+  /* rankUpUsingCash(Player player)
+  * takes a player and ranks
+  */
+  public static void rankUpUsingCash(Player player, int desiredRank){
+    int cash = player.getCash();
+    if(desiredRank * 5 <= cash){
+      player.setCash(cash - currRank * 5);
+      player.setRank(currRank + 1);
 
+    }
   }
+  public static void rankUpUsingFame(Player player, int desiredRank){
+    int fame = player.getFame();
+    int reqFame = fameNeeded(desiredRank, 0);
+
+    if(fame >= reqFame){
+      player.setFame(fame - reqFame);
+      player.setRank(currRank + 1);
+
+    }
+  }
+  private static int fameNeeded(int rank, int fame){
+    int ret;
+
+
+    if(rank <= 1){
+      return 4;
+    }
+    else{
+      ret =  fameNeeded(rank -1 , fame) +(rank * 2) + 2;
+    }
+    return ret;
+  }
+
 
 }
