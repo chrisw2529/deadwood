36cf09933bcdbd1ded04123bb24cce40e5d2fbfc
chrisw2529 <chrisw2529@gmail.com>
2018-05-14 16:23:27 -0700

    working on wrapscene

--- Scene.java
+++ Scene.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Scene extends Space
 {
@@ -20,7 +21,13 @@ public class Scene extends Space
 
     public static void wrapScene()
     {
-
+      int[] payout = new int[budget];
+      for(int i = 0; i< payout.size(); i++){
+        payout[i] = roleDie();
+      }
+      Arrays.sort(payout);
+      System.out.printf("Modified arr[] : %s", Arrays.toString(arr));
+      
     }
 
     public int getShotMarker(){
