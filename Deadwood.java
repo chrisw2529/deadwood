b98bdae786b1fbc86cf6013de01ec801e09a13ea
chrisw2529 <chrisw2529@gmail.com>
2018-05-15 17:01:51 -0700

    added parser stuff

--- Deadwood.java
+++ Deadwood.java
@@ -1,7 +1,37 @@
+import java.util.Random;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+
 public class Deadwood{
   public static void main(String[] args){
 
     Player joe = new Player(1);
-    
+    ParseXML p = new ParseXML();
+    File board = new File("board.xml");
+    // DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    // DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    // Document doc = dBuilder.parse(board);
+    // doc.getDocumentElement().normalize();
+    try {
+      Document d = p.getDocFromFile("board.xml");
+      p.readSceneData(d);
+    }
+    catch(ParserConfigurationException ex) {
+      System.out.println("par C E");
+
+    }
+    //readSceneData(d);
   }
+
+
 }
