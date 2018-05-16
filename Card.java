9debf151ec1f54f6faa6cabb441b7db80904fae3
chrisw2529 <chrisw2529@gmail.com>
2018-05-15 19:46:57 -0700

    working on wrap scene

--- Card.java
+++ Card.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Arrays;
-
+import java.util.*;
+import java.lang.*;
 public class Card
 {
     String name;
@@ -54,8 +55,17 @@ public class Card
         payout[i] = Board.roleDie();
       }
       Arrays.sort(payout);
-      System.out.printf("Modified arr[] : %s", Arrays.toString(payout));
+      for(int i = 0; i < payout.length / 2; i++)
+      {
+        int temp = payout[i];
+        payout[i] = payout[payout.length - i - 1];
+        payout[payout.length - i - 1] = temp;
+      }
+      //Collections.reversep(Arrays.asList(payout));
+      System.out.printf("Modified arr[] : %s\n", Arrays.toString(payout));
+      for(int i = payout.length - 1; i >= 0; i--){
 
+      }
     }
 
 }
