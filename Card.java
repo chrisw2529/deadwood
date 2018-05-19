a7e03075032cdc87d0e7d7f7a99fb96e9b8f5a3c
chrisw2529 <chrisw2529@gmail.com>
2018-05-18 18:19:16 -0700

    DONEgit add *.javagit add *.javagit add *.javagit add *.java!

--- Card.java
+++ Card.java
@@ -69,39 +69,63 @@ public class Card
     //$$$$$$$
     public void wrapScene(Board board)
     {
-      int[] payout = new int[getBudget()];
-      for(int i = 0; i< payout.length; i++){
-        payout[i] = Board.roleDie();
-      }
-      Arrays.sort(payout);
-      for(int i = 0; i < payout.length / 2; i++)
-      {
-        int temp = payout[i];
-        payout[i] = payout[payout.length - i - 1];
-        payout[payout.length - i - 1] = temp;
-      }
-      //Collections.reversep(Arrays.asList(payout));
-      System.out.printf("Modified arr[] : %s\n", Arrays.toString(payout));
-      int count = 0;
-      //payout on card roles
-      while(count < payout.length){
-        for(int j = roles.size() - 1; j >= 0; j--){
-          Player payTo = roles.get(j).getPlayer();
-          if(payTo != null){
-            payTo.setCash(payTo.getCash() + payout[count]);
-            System.out.println("player " + j + "cash increased by " + payout[count]);
+      //off card bonuses
+      for (int i = 0 ; i < set.getRoles().size() ;i++ ) {
+        if(set.getRoles().get(i).getPlayer() != null){
+          int curCash = set.getRoles().get(i).getPlayer().getCash();
+          set.getRoles().get(i).getPlayer().setCash(curCash + budget);
+          System.out.println("distributing off card bonus player " + set.getRoles().get(i).getPlayer().getID()+ "'s cash incresed by " + budget+ " dollars");
 
-          }
-          count++;
-          if(count >= payout.length){
-            break;
+        }
+      }
+
+      //on card bonuses
+      int[] payout = new int[getBudget()];
+      boolean someOneOnCard = false;
+      for(int i = 0 ; i < roles.size(); i++){
+        if(roles.get(i).getPlayer() != null){
+          someOneOnCard = true;
+        }
+      }
+      if(someOneOnCard){
+        System.out.println("distributing on card bonuses");
+        for(int i = 0; i< payout.length; i++){
+          payout[i] = Board.roleDie();
+        }
+        Arrays.sort(payout);
+        //reverse array order
+        for(int i = 0; i < payout.length / 2; i++)
+        {
+          int temp = payout[i];
+          payout[i] = payout[payout.length - i - 1];
+          payout[payout.length - i - 1] = temp;
+        }
+        //Collections.reversep(Arrays.asList(payout));
+        System.out.printf("The die rolls where : %s\n", Arrays.toString(payout));
+        int count = 0;
+        //payout on card roles
+        while(count < payout.length){
+          for(int j = roles.size() - 1; j >= 0; j--){
+            Player payTo = roles.get(j).getPlayer();
+            if(payTo != null){
+              payTo.setCash(payTo.getCash() + payout[count]);
+              System.out.println("player " + (j+1) + "'s cash increased by " + payout[count]);
+
+            }
+            count++;
+            if(count >= payout.length){
+              break;
+            }
           }
         }
       }
+      else{
+        System.out.println("no one was on card no on-card bonuses");
+      }
+
       //reset roles for off scene
       for (int i = 0; i < set.getRoles().size(); i++) {
         if(set.getRoles().get(i).getPlayer() != null){
-          System.out.println("player ID is: " + set.getRoles().get(i).getPlayer().getID());
           set.getRoles().get(i).getPlayer().setRehearsal(0);
           set.getRoles().get(i).getPlayer().setRole(null);
           set.getRoles().get(i).setPlayer(null);
